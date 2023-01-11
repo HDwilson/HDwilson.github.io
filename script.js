10934f7c6a083b53cfb48b75687abf787fdab4e4
HDwilson <willywilsonhd+github@gmail.com>
2023-01-11 22:23:02 +0800

    modified:   index.html 	new file:   script.js 	modified:   styles.css

---
+++ script.js
@@ -0,0 +1,10 @@
+let yesButton = document.getElementById("yes-button");
+let noButton = document.getElementById("no-button");
+
+yesButton.addEventListener("click", function() {
+    alert("You clicked the 'Yes' button!");
+});
+
+noButton.addEventListener("click", function() {
+    alert("You clicked the 'No' button!");
+});
